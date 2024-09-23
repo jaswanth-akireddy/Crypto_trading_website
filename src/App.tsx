1685632166b79d0ae0953d8fd2f6dfad173f8630
Akireddy Jaswanth <19pa1a0501@vishnu.edu.in>
2024-09-23 20:01:26 +0530

    added some more

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from 'react'
 import "./App.css";
 import TradeScreen from './pages/tradeScreen';
-import { Route, Routes, HashRouter } from 'react-router-dom'; // Import HashRouter
+import { Route, Routes} from 'react-router-dom';
 import DashBoard from './pages/dashBoardScreen';
 import PaymentSuccessfulScreen from './pages/paymentSuccessful';
 import PurchaseScreen from './pages/purchaseScreen';
@@ -9,22 +9,21 @@ import { Provider } from 'react-redux';
 import store from './Components/store';
 import { ThemeProvider } from '@mui/material';
 import theme from './themes';
-
 function App() {
   return (
+    <>
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <HashRouter> {/* Use HashRouter here */}
-          <Routes>
-            <Route path='/' element={<DashBoard />} />
-            <Route path='/TradeScreen' element={<TradeScreen />} />
-            <Route path='/purchase-screen' element={<PurchaseScreen />} />
-            <Route path='/payment-successfull' element={<PaymentSuccessfulScreen successType='buy' price='0.0234510 BTC' />} />
-          </Routes>
-        </HashRouter>
+    <ThemeProvider theme={theme}>
+       <Routes>
+    <Route path='/' element={<DashBoard/>}/> 
+    <Route path='/TradeScreen' element={<TradeScreen/>}/>
+    <Route path='/purchase-screen' element={<PurchaseScreen/>}/>
+    <Route path='/payment-successfull' element={<PaymentSuccessfulScreen successType='buy' price='0.0234510 BTC'/>}/>
+      </Routes>
       </ThemeProvider>
-    </Provider>
+      </Provider>
+     
+    </>
   );
 };
-
 export default App;
